497e3d63ffdd6e3137e9694bab69ed7dfc9b6ec2
宿梓航 <Ferdinandsukhoi@outlook.com>
2019-09-14 15:22:00 +0800

    Initial.

---
+++ PowerSSHCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PowerSSHCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
